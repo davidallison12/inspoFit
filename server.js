d9773f0f399f4328abde44a5767d47781c5b6758
david-allison12 <dgreenal@gmail.com>
2021-09-22 18:41:01 -0400

    Created base express structure | Now listening on port

---
+++ server.js
@@ -0,0 +1,46 @@
+//=================
+// REQUIRMENTS
+//=================
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const PORT = 3000
+
+
+
+//=================
+// MODEL
+//=================
+
+
+
+
+
+
+
+//=================
+// MIDDLEWARE
+//=================
+
+
+
+
+
+
+
+//=================
+// CONTROLLERS
+//=================
+
+
+
+
+
+
+//=================
+// LISTENER
+//=================
+
+app.listen(PORT, (req, res) => {
+    console.log(`Now listening on port ${PORT}... 🥳`)
+})
