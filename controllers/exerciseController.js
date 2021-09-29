a8a9b49f22e563c2bb8742de101dbad2a0f147b8
david-allison12 <dgreenal@gmail.com>
2021-09-28 22:56:15 -0400

    Index route is rendering and showing exercise data

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -6,13 +6,51 @@ const Exercise = require('../models/exercises')
 
 
 // Seed 
-
+router.get('/seed', async (req, res) => {
+    const newExercises = [
+    {
+        name: 'Squat',
+        sets: "3",
+        reps: "12-15",
+        equipment: "Barbell",
+        vidDescription:"https://www.youtube.com/watch?v=bEv6CCg2BC8",
+        isSuperset: false
+    },{
+        name: 'Chest Flyes',
+        sets: "3",
+        reps: "12-15",
+        equipment: "Dumbbells",
+        vidDescription:"https://www.youtube.com/watch?v=QENKPHhQVi4",
+        isSuperset: true
+    },{
+        name: 'Seal Row',
+        sets: "5",
+        reps: "12-15",
+        equipment: "Barbell, Bench",
+        vidDescription:"https://www.youtube.com/watch?v=fBgDGkfT8Rc",
+        isSuperset: true
+    },{
+        name: 'Split Squats',
+        sets: "4",
+        reps: "15",
+        equipment:"",
+        vidDescription:"https://www.youtube.com/watch?v=ZZgp-zZQPMc",
+        isSuperset: true
+    }
+    ]
+    try {
+        const seedData = await Exercise.create(newExercises)
+        res.send(seedData)
+    } catch (err) {
+        res.send(err.message)
+    }
+})
 
 
 // Index
 router.get('/', (req, res) => {
     Exercise.find({}, (err, foundExercises) => {
-        res.send(foundExercises)
+        res.render('exercises/index.ejs', {exercises: foundExercises})
     })
 })
 
